25c3df4290e5c3ed8c458fe143c8b892717591c8
LoneKingCode <hackerbest@foxmail.com>
2018-01-05 16:10:52 +0800

    。。

--- src/lkWeb/Areas/Admin/Controllers/Base/AdminBaseController.cs
+++ src/lkWeb/Areas/Admin/Controllers/Base/AdminBaseController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using lkWeb.Service.Dto;
 using lkWeb.Service.Abstracts;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -22,5 +23,16 @@ namespace lkWeb.Areas.Admin.Controllers
         /// 是否登录
         /// </summary>
         protected bool IsLogined { get; set; }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            //再加上判断菜单权限
+            //context.HttpContext.Session.TryGetValue("CurrentUser", out byte[] result);
+            //if (result == null)
+            //{
+            //    context.Result = new RedirectToActionResult("Login", "User", "0");
+            //}
+            base.OnActionExecuting(context);
+        }
     }
 }
