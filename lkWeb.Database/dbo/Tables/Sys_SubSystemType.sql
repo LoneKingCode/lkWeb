3cb7c19826a3b4c42e8f29360b9f5c62272d1739
lone5402 <lone5402@outlook.com>
2019-04-21 22:52:24 +0800

    修改项目结构

--- lkWeb.Database/dbo/Tables/Sys_SubSystemType.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Sys_SubSystemType] (
-    [Id]             INT           IDENTITY (1, 1) NOT NULL,
-    [CreateDateTime] DATETIME2 (7) NOT NULL,
-    [Description]    VARCHAR (30)  NOT NULL,
-    [Name]           VARCHAR (18)  NOT NULL,
-    CONSTRAINT [PK_Sys_SubSystemType] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
